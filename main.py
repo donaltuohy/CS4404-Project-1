95f45dd688c42bc3f075c55606d553f0c9b1327a
stefano <lupos@tcd.ie>
2017-10-18 23:04:05 +0100

    Basic Linear Regression

--- main.py
+++ main.py
@@ -1,38 +1,57 @@
-
-# Python
 import tensorflow as tf
-node1 = tf.constant([3.0,4], dtype=tf.float32)
-node2 = tf.constant([4.0, 5.0]) # also tf.float32 implicitly
-print(node1, node2)
+# NumPy is often used to load, manipulate and preprocess data.
+import numpy as np
+
+import matplotlib.pyplot as plot
+
+# Read in the data
+# my_data = np.genfromtxt('houses_10k.csv', delimiter=',', dtype=None)
+houseData = np.recfromcsv('houses_10k.csv', delimiter=',', filling_values=np.nan, case_sensitive=True, deletechars='', replace_space=' ')
+
+houseData = houseData[:100]
+
+numInstances = len(houseData)
+
+trainData = houseData[:round(numInstances/2)]
+testData =  houseData[round(numInstances/2):]
+
+# plot.plot(testData['sqft_living'], testData['price'], 'ro')
+# plot.show()
 
 
 
-sess = tf.Session()
-print(sess.run([node1, node2]))
+# Declare list of features. We only have one numeric feature. There are many
+# other types of columns that are more complicated and useful.
+feature_columns = [tf.feature_column.numeric_column("x", shape=[1])]
 
-node3 = tf.add(node1, node2)
-print("node3:", node3)
-print("sess.run(node3):", sess.run(node3))
+# An estimator is the front end to invoke training (fitting) and evaluation
+# (inference). There are many predefined types like linear regression,
+# linear classification, and many neural network classifiers and regressors.
+# The following code provides an estimator that does linear regression.
+estimator = tf.estimator.LinearRegressor(feature_columns=feature_columns)
 
+# TensorFlow provides many helper methods to read and set up data sets.
+# Here we use two data sets: one for training and one for evaluation
+# We have to tell the function how many batches
+# of data (num_epochs) we want and how big each batch should be.
+x_train = np.array(trainData['sqft_living'])
+y_train = np.array(trainData['price'])
+x_eval = np.array(testData['sqft_living'])
+y_eval = np.array(testData['price'])
+input_fn = tf.estimator.inputs.numpy_input_fn(
+    {"x": x_train}, y_train, batch_size=4, num_epochs=None, shuffle=True)
+train_input_fn = tf.estimator.inputs.numpy_input_fn(
+    {"x": x_train}, y_train, batch_size=4, num_epochs=1000, shuffle=False)
+eval_input_fn = tf.estimator.inputs.numpy_input_fn(
+    {"x": x_eval}, y_eval, batch_size=4, num_epochs=1000, shuffle=False)
 
-a = tf.placeholder(tf.float32)
-b = tf.placeholder(tf.float32)
-adder_node = a + b  # + provides a shortcut for tf.add(a, b)
+# We can invoke 1000 training steps by invoking the  method and passing the
+# training data set.
+estimator.train(input_fn=input_fn, steps=1000)
 
-print(sess.run(adder_node, {a: 3, b: 4.5}))
-print(sess.run(adder_node, {a: [1, 3], b: [2, 4]}))
+# Here we evaluate how well our model did.
+train_metrics = estimator.evaluate(input_fn=train_input_fn)
+eval_metrics = estimator.evaluate(input_fn=eval_input_fn)
+print("train metrics: %r"% train_metrics)
+print("eval metrics: %r"% eval_metrics)
 
-add_and_triple = adder_node * 3.
-print(sess.run(add_and_triple, {a: 3, b: 4.5}))
-
-
-# Linear Regression
-W = tf.Variable([1], dtype=tf.float32)
-b = tf.Variable([10], dtype=tf.float32)
-x = tf.placeholder(tf.float32)
-linear_model = W * x + b
-
-init = tf.global_variables_initializer()
-sess.run(init)
-
-print(sess.run(linear_model, {x: [1, 2, 3, 4]}))
