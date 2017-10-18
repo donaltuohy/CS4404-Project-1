8c871020f88f9a1b7f0874893a70cee5bcbd773f
stefano <lupos@tcd.ie>
2017-10-18 13:51:35 +0100

    Basic Tensor Flow

---
+++ main.py
@@ -0,0 +1,38 @@
+
+# Python
+import tensorflow as tf
+node1 = tf.constant([3.0,4], dtype=tf.float32)
+node2 = tf.constant([4.0, 5.0]) # also tf.float32 implicitly
+print(node1, node2)
+
+
+
+sess = tf.Session()
+print(sess.run([node1, node2]))
+
+node3 = tf.add(node1, node2)
+print("node3:", node3)
+print("sess.run(node3):", sess.run(node3))
+
+
+a = tf.placeholder(tf.float32)
+b = tf.placeholder(tf.float32)
+adder_node = a + b  # + provides a shortcut for tf.add(a, b)
+
+print(sess.run(adder_node, {a: 3, b: 4.5}))
+print(sess.run(adder_node, {a: [1, 3], b: [2, 4]}))
+
+add_and_triple = adder_node * 3.
+print(sess.run(add_and_triple, {a: 3, b: 4.5}))
+
+
+# Linear Regression
+W = tf.Variable([1], dtype=tf.float32)
+b = tf.Variable([10], dtype=tf.float32)
+x = tf.placeholder(tf.float32)
+linear_model = W * x + b
+
+init = tf.global_variables_initializer()
+sess.run(init)
+
+print(sess.run(linear_model, {x: [1, 2, 3, 4]}))
