4d5b3d1d7263ab04c203b26afbc11fdd125f8609
Stefano <lupos@tcd.ie>
2017-10-22 00:06:09 +0100

    Bug fix and tidied code for linear regression

--- config.py
+++ config.py
@@ -1,8 +1,5 @@
-import os
-
-
 TRAINING_PARAMS = dict(
-    NORMALIZE_METHOD = "SCALE",              # How features should be normalized
+    NORMALIZE_METHOD = "ZSCORE",             # How features should be normalized
     DESIRED_NUM_INSTANCES = None,            # Specify max number of instances (None uses all instances)
     SPLIT_METHOD = "70/30",                  # One of "70/30" or "10Fold"
     LEARNING_RATE = 0.1,                     # Stepsize for gradient descent
@@ -10,25 +7,35 @@ TRAINING_PARAMS = dict(
 )
 
 
+# Specify a list of features using the FEATURES field,
+# If these features are to be ommited then set OMIT_FEATURES to True
+# If they are to be the features used for the inputs, set OMIT_FEATURES to False
+
 # Dataset 1 - SUM with noise
 SUM_WITH_NOISE = dict(
-    FILE_NAME = "SUM_with_noise.csv",
-    OMITTED_FEATURES = ["Instance", "Noisy Target", "Noisy Target Class"],
+    FILE_NAME = "sum_with_noise.csv",
+    DELIMETER = ";",
+    FEATURES = ["Instance", "Noisy Target", "Noisy Target Class"],
+    OMIT_FEATURES = True,
     LABEL = "Noisy Target"
 )
 
 # Dataset 2 - House Data
 HOUSE_DATA = dict(
-    FILE_NAME = "SUM_with_noise.csv",
-    OMITTED_FEATURES = ["Instance", "Noisy Target", "Noisy Target Class"],
-    LABEL = "Noisy Target"
+    FILE_NAME = "housing_data.csv",
+    DELIMETER = ",",
+    FEATURES = ["LotArea", "OverallQual", "OverallCond", "BedroomAbvGr"],
+    OMIT_FEATURES = False,
+    LABEL = "SalePrice"
 )
 
 # Dataset 3 - 10k of SUM without noise (for faster debugging)
 SUM_10K_WITHOUT_NOISE = dict(
-    FILE_NAME = "SUM_10k_without_noise.csv",
-    OMITTED_FEATURES = ["Instance", "Target", "Target Class"],
+    FILE_NAME = "sum_10k_without_noise.csv",
+    DELIMETER = ";",
+    FEATURES = ["Instance", "Target", "Target Class"],
+    OMIT_FEATURES = True,
     LABEL = "Target"
 )
 
-ACTIVE_DATASET = SUM_10K_WITHOUT_NOISE
+ACTIVE_DATASET = HOUSE_DATA
--- main.py
+++ main.py
@@ -19,32 +19,34 @@ def printM(dataset):
 
 # Read's data from CSV File
 def readData():
-    csvData = np.recfromcsv(ACTIVE_DATASET['FILE_NAME'], delimiter=';', filling_values=np.nan, case_sensitive=True, deletechars='', replace_space=' ')
+    csvData = np.recfromcsv(ACTIVE_DATASET['FILE_NAME'], delimiter=ACTIVE_DATASET['DELIMETER'], filling_values=np.nan, case_sensitive=True, deletechars='', replace_space=' ')
     numInstances = TRAINING_PARAMS['DESIRED_NUM_INSTANCES'] or len(csvData)
     csvData = csvData[:numInstances]
     return csvData
 
 
 # Finds the column numbers of the features to be included in the design matrix X
-def getDesiredFeatureIndices(allFeatureNames, ommitedFeatureNames):
-    return [i for i, x in enumerate(allFeatureNames) if x not in ommitedFeatureNames]
+def getDesiredFeatureIndices(allFeatureNames, features, omitFeatures):
+    return [i for i, x in enumerate(allFeatureNames) if ((x in features and not omitFeatures) or (x not in features and omitFeatures))]
 
 
 # Builds the design matrix X inlcuding a column vector of 1s for the bias terms
-def createDesignMatrix(dataset, omittedFeatures):
-    numDesignMatrixFeatures = len(dataset[0]) - len(omittedFeatures) + 1    # include column of 1s for bias
-    X = np.zeros((len(dataset), numDesignMatrixFeatures))
+def createDesignMatrix(dataset, features, omitFeatures):
+    if(ACTIVE_DATASET['OMIT_FEATURES']):
+        numDesignMatrixFeatures = len(dataset[0]) - len(features)
+    else:
+        numDesignMatrixFeatures = len(features)
+    X = np.ones((len(dataset), numDesignMatrixFeatures))
 
-    featureIndices = getDesiredFeatureIndices(list(dataset.dtype.names), omittedFeatures)
+    featureIndices = getDesiredFeatureIndices(list(dataset.dtype.names), features, omitFeatures)
 
-    X[:,0] = 1
-    currentCol = 1
+    currentCol = 0
     for i, row in enumerate(dataset):
         for j, col in enumerate(row):
             if(j in featureIndices):
                 X[i,currentCol] = col
                 currentCol = currentCol + 1
-        currentCol = 1
+        currentCol = 0
     return X
 
 
@@ -61,15 +63,13 @@ def splitData(X, y):
         yTrain = y[:numTrainInstances]
         xTest = X[numTrainInstances:]
         yTest = y[numTrainInstances:]
-    return xTrain, yTrain, xTest, yTest
+        return xTrain, yTrain, xTest, yTest
 
 
 
 # Normalizes features to Standard Normal Variable or maps them over [0,1] range
 def featureNormalize(dataset):
-    if(TRAINING_PARAMS['NORMALIZE_METHOD']):
-        return scale(dataset)
-    elif(NORMALIZE_METHOD == "MINMAX"):
+    if(TRAINING_PARAMS['NORMALIZE_METHOD'] == "MINMAX"):
         print("Using min-max normalization")
         mins = np.amin(dataset, axis=0)
         maxs = np.amax(dataset, axis=0)
@@ -80,15 +80,41 @@ def featureNormalize(dataset):
         print("Using Z-Score Normalization with mean = ", mu, ", std dev = ", sigma)
         return (dataset-mu)/sigma
 
+# Prepends a column of ones to design matrix to represent bias term 
+def prependBiasTerm(dataset):
+  numInstances = dataset.shape[0]
+  numFeatures = dataset.shape[1]
+  X = np.reshape(np.c_[np.ones(numInstances),dataset],[numInstances,numFeatures + 1])
+  return X
 
+ # Plot some of the training data against input features
+def showPlots(X, y):
+    for i in range(1,X.shape[1]):
+        plt.plot(X[:,i], y, 'r+')
+        plt.xlabel("Feature %d" % i)
+        plt.ylabel(ACTIVE_DATASET['LABEL'])
+        plt.show()
+
+
+
+###########################################
+####     Begin Data Organization    #######
+###########################################
 
 data = readData()
-X = createDesignMatrix(data, ACTIVE_DATASET['OMITTED_FEATURES'])
+X = createDesignMatrix(data, ACTIVE_DATASET['FEATURES'], ACTIVE_DATASET['OMIT_FEATURES'])
 y = createLabelVector(data, ACTIVE_DATASET['LABEL'])
+
 
 X = featureNormalize(X)
 y = featureNormalize(y)
+
+X = prependBiasTerm(X)
+
 xTrain, yTrain, xTest, yTest = splitData(X, y)
+
+# print(xTrain)
+# showPlots(X,y)
 
 numFeatures = len(xTrain[0])    # inclusive of bias column vector
 numTrainInstances = len(yTrain)
@@ -102,15 +128,15 @@ numTestInstances = len(yTest)
 X = tf.placeholder(tf.float32, [None, numFeatures])
 Y = tf.placeholder(tf.float32,[None,1])
 W = tf.Variable(tf.ones([numFeatures, 1]))
-cost_history = np.empty(shape=[1],dtype=float)
+lossFunctionHistory = np.empty(shape=[1],dtype=float)
 
 
 
 # Specify Linear Regression method
 init = tf.global_variables_initializer()
-y_ = tf.matmul(X, W)
-cost = tf.reduce_mean(tf.square(Y-y_))
-training_step = tf.train.GradientDescentOptimizer(TRAINING_PARAMS['LEARNING_RATE']).minimize(cost)
+yPredictor = tf.matmul(X, W)
+cost = tf.reduce_mean(tf.square(Y-yPredictor))
+trainingStep = tf.train.GradientDescentOptimizer(TRAINING_PARAMS['LEARNING_RATE']).minimize(cost)
 
 # Run the session
 sess = tf.Session()
@@ -118,47 +144,48 @@ sess.run(init)
 
 # Train
 for epoch in range(TRAINING_PARAMS['TRAINING_EPOCHS']):
-    sess.run(training_step,feed_dict={X:xTrain,Y:yTrain})
-    cost_history = np.append(cost_history, sess.run(cost,feed_dict={X: xTrain,Y: yTrain}))
+    sess.run(trainingStep,feed_dict={X:xTrain,Y:yTrain})
+    lossFunctionHistory = np.append(lossFunctionHistory, sess.run(cost,feed_dict={X: xTrain,Y: yTrain}))
 
 
 ###########################################
 ####         Evaluation             #######
 ###########################################
 # Plot the cost function over time
-plt.plot(range(len(cost_history)), cost_history, 'b+')
+plt.plot(range(len(lossFunctionHistory)), lossFunctionHistory, 'b+')
 plt.xlabel("Epoch #")
 plt.ylabel("Cost function")
-plt.axis([0,TRAINING_PARAMS['TRAINING_EPOCHS'],0,np.max(cost_history) + (0.1*np.max(cost_history))])
+plt.axis([0,TRAINING_PARAMS['TRAINING_EPOCHS'],0,np.max(lossFunctionHistory) + (0.1*np.max(lossFunctionHistory))])
 plt.show()
 
 # Predict Y Values for given test values
-pred_y = sess.run(y_, feed_dict={X: xTest})
+predictedY = sess.run(yPredictor, feed_dict={X: xTest})
 
 # Calculate the Mean Square Error
-mse = tf.reduce_mean(tf.square(pred_y - yTest))
+mse = tf.reduce_mean(tf.square(predictedY - yTest))
 
 # Print Results
 weights = np.matrix(sess.run(W))
-print("MSE: %.4f" % sess.run(mse))
+print("Minimum Loss Function Value:", np.min(lossFunctionHistory),", MSE: %.4f" % sess.run(mse))
 print("Weights: ", weights)
 
 
-## 1 Feature LR plot
-# plt.plot(xTest[:,1], yTest, 'ro')
-# plt.plot(xTest[:,1], pred_y, 'bx')
-# plt.xlabel("Square Footage")
-# plt.ylabel("Price")
-# plt.show()
+# Plot first feature against predicter / measured values
+plt.plot(xTest[:,1], yTest, 'ro')
+plt.plot(xTest[:,1], predictedY, 'bx')
+plt.xlabel("Feature 1")
+plt.ylabel(ACTIVE_DATASET['LABEL'])
+plt.show()
 
-## Multi feature LR Plot
-plt.plot(pred_y, yTest, 'ro')
+# Plot Predicted y values against measured y values
+plt.plot(predictedY, yTest, 'ro')
 
-if((TRAINING_PARAMS['NORMALIZE_METHOD'] == "MINMAX") or(TRAINING_PARAMS['NORMALIZE_METHOD'] == "SCALE")):
-    plt.plot([0, 1, 2])
-    plt.axis([0, 1.5, 0, 1.5])
+if((TRAINING_PARAMS['NORMALIZE_METHOD'] == "MINMAX")):
+    # Plot over range from [0,1]
+    plt.plot([0, 1])
 else:
+    # Plot over range [-3, 3] ~ 99 % of data
     plt.plot(range(-3,3), range(-3,3), 'b')
-plt.xlabel("Predicted Price")
-plt.ylabel("Actual Price")
+plt.xlabel("Predicted")
+plt.ylabel("Actual")
 plt.show()
